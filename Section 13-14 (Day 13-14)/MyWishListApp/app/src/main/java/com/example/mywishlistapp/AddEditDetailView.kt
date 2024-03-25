63b7ce4273521c7c9e263ee672ca50e10b02a013
alemivas <hskthefglab9@mail.ru>
2024-03-25 07:06:56 +0400

    add result of 6 week

--- Section 13-14 (Day 13-14)/MyWishListApp/app/src/main/java/com/example/mywishlistapp/AddEditDetailView.kt
+++ Section 13-14 (Day 13-14)/MyWishListApp/app/src/main/java/com/example/mywishlistapp/AddEditDetailView.kt
@@ -9,11 +9,16 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -25,6 +30,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.example.mywishlistapp.data.Wish
+import kotlinx.coroutines.launch
 
 @Composable
 fun AddEditDetailView(
@@ -32,10 +39,27 @@ fun AddEditDetailView(
     viewModel: WishViewModel,
     navController: NavController
 ){
+
+    val snackMessage = remember {
+        mutableStateOf("")
+    }
+    val scope = rememberCoroutineScope()
+    val scaffoldState = rememberScaffoldState()
+
+    if (id != 0L){
+        val wish = viewModel.getAWishById(id).collectAsState(initial = Wish(0L, "", ""))
+        viewModel.wishTitleState = wish.value.title
+        viewModel.wishDescriptionState = wish.value.description
+    }else{
+        viewModel.wishTitleState = ""
+        viewModel.wishDescriptionState = ""
+    }
+
     Scaffold(
         topBar = { AppBarView(title =
             if (id != 0L) stringResource(id = R.string.update_wish) else stringResource(id = R.string.add_wish)
         ) {navController.navigateUp()} },
+        scaffoldState = scaffoldState,
     ) {
         Column(
             modifier = Modifier
@@ -70,9 +94,29 @@ fun AddEditDetailView(
                 if(
                     viewModel.wishTitleState.isNotEmpty() &&
                     viewModel.wishDescriptionState.isNotEmpty()){
-                    //TODO UpdateWish
+                    if (id != 0L){
+                        viewModel.updateWish(
+                            Wish(
+                                id = id,
+                                title = viewModel.wishTitleState.trim(),
+                                description = viewModel.wishDescriptionState.trim()
+                            )
+                        )
+                    }else{
+                        viewModel.addWish(
+                            Wish(
+                                title = viewModel.wishTitleState.trim(),
+                                description = viewModel.wishDescriptionState.trim()
+                            )
+                        )
+                        snackMessage.value = "Wish created"
+                    }
                 }else{
-                    // TODO AddWish
+                    snackMessage.value = "Enter wish fields"
+                }
+                scope.launch {
+                    scaffoldState.snackbarHostState.showSnackbar(snackMessage.value)
+                    navController.navigateUp()
                 }
             }) {
                 Text(
--- Section 13-14 (Day 13-14)/MyWishListApp/app/src/main/java/com/example/mywishlistapp/HomeView.kt
+++ Section 13-14 (Day 13-14)/MyWishListApp/app/src/main/java/com/example/mywishlistapp/HomeView.kt
@@ -8,15 +8,24 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.DismissValue
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.FractionalThreshold
+import androidx.compose.material.Scaffold
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.rememberDismissState
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardElevation
+import androidx.compose.material3.DismissDirection
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SwipeToDismiss
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
@@ -26,15 +35,18 @@ import androidx.navigation.NavController
 import com.example.mywishlistapp.data.DummyWish
 import com.example.mywishlistapp.data.Wish
 
+@OptIn(ExperimentalMaterialApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun HomeView(
     navController: NavController,
     viewModel: WishViewModel
 ){
     val context = LocalContext.current
+    val scaffoldState = rememberScaffoldState()
     Scaffold(
+        scaffoldState = scaffoldState,
         topBar = { AppBarView(title = "WhishList", {
-            Toast.makeText(context, "Button clicked", Toast.LENGTH_LONG).show()
+//            Toast.makeText(context, "Button clicked", Toast.LENGTH_LONG).show()
         })},
         floatingActionButton = {
             FloatingActionButton(
@@ -43,21 +55,42 @@ fun HomeView(
 //                backgroundColor = Color.Black,
                 onClick = {
 //                    Toast.makeText(context, "Button clicked", Toast.LENGTH_LONG).show()
-                    navController.navigate(Screen.AddScreen.route)
+                    navController.navigate(Screen.AddScreen.route + "/0L")
                 }) {
                 Icon(imageVector = Icons.Default.Add, contentDescription = null)
             }
         }
     ) {
+        val wishlist = viewModel.getAllWishes.collectAsState(initial = listOf())
         LazyColumn(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(it)
         ) {
-            items(DummyWish.wishList){
-                wish -> WishItem(wish = wish) {
-
-                 }
+//            items(DummyWish.wishList){
+            items(wishlist.value){
+                wish ->
+                val dismissState = rememberDismissState(
+                    confirmStateChange = {
+                        if(it == DismissValue.DismissedToEnd || it == DismissValue.DismissedToStart){
+                            viewModel.deleteWish(wish)
+                        }
+                        true
+                    }
+                )
+                
+                SwipeToDismiss(
+                    state = dismissState,
+                    background = {},
+                    directions = setOf(DismissDirection.StartToEnd, DismissDirection.EndToStart),
+                    dismissThresholds = {FractionalThreshold(0.25f)},
+                    dismissContent = {}
+                )
+                
+                WishItem(wish = wish) {
+                    val id = wish.id
+                    navController.navigate(Screen.AddScreen.route + "/$id")
+                }
             }
         }
     }
