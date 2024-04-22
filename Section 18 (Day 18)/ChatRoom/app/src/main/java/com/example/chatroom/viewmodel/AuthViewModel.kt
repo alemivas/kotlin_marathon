129c57e6684287ee9afad08af4ac09118e12ce17
alemivas <hskthefglab9@mail.ru>
2024-04-22 05:36:40 +0400

    Section 18 (Day 18)

---
+++ Section 18 (Day 18)/ChatRoom/app/src/main/java/com/example/chatroom/viewmodel/AuthViewModel.kt
@@ -0,0 +1,37 @@
+package com.example.chatroom.viewmodel
+
+import com.example.chatroom.data.Result
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.chatroom.Injection
+import com.example.chatroom.data.UserRepository
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.coroutines.launch
+
+class AuthViewModel : ViewModel() {
+    private val userRepository: UserRepository
+
+    init {
+        userRepository = UserRepository(
+            FirebaseAuth.getInstance(),
+            Injection.instance()
+        )
+    }
+
+    private val _authResult = MutableLiveData<Result<Boolean>>()
+    val authResult: LiveData<Result<Boolean>> get() = _authResult
+
+    fun signUp(email: String, password: String, firstName: String, lastName: String) {
+        viewModelScope.launch {
+            _authResult.value = userRepository.signUp(email, password, firstName, lastName)
+        }
+    }
+
+    fun login(email: String, password: String) {
+        viewModelScope.launch {
+            _authResult.value = userRepository.login(email, password)
+        }
+    }
+}
